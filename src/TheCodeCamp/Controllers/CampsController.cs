ab6c79192017832bdeb150d3efdcc0bc78bfcccb
Matthew Dodi <matthew.dodi@gmail.com>
2019-03-29 21:48:15 -0700

    Type: feat - Created new CampController that handled a route

---
+++ src/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        // GET: Camps
+        public object Get()
+        {
+            return new { name = "Matthew", Occupation = "Developer" };
+        }
+    }
+}
